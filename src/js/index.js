96bfb0654ca9c15f73fe22b8314744327d919d25
Andrey <shock.work@mail.ru>
2020-12-30 21:52:08 +0300

    close menu after click burger

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 const burger = document.querySelector('.burger')
 const menu = document.querySelector('.menu__list')
+const overlay = document.querySelector('.overlay')
 
-burger.addEventListener('click', () => {
-    menu.classList.add('active')
-})
+const toggleElements = (action = 'add', className = 'active') => {
+    overlay.classList[action](className)
+    menu.classList[action](className)
+}
+
+burger.addEventListener('click', () => { toggleElements() })
+overlay.addEventListener('click', () => { toggleElements('remove') })
