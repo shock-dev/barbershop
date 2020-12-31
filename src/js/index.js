e72827b12a3320c6c6023de1cfcdf2acc36bfc9d
Andrey <shock.work@mail.ru>
2021-01-01 01:03:44 +0300

    function for lock DOM body

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 const burger = document.querySelector('.burger')
 const menu = document.querySelector('.menu__list')
 const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal__wrapper')
 const loginButtonsFromMenu = document.querySelectorAll('.header__btn')
 const closeModalButton = document.querySelector('.modal__close')
 
+const isScrollingAvailable = (q = true) => {
+    document.body.classList[q ? 'add' : 'remove']('lock')
+}
 
