2fffc109b5675eef7513a6fe35fb24eb7cbfcbe8
Andrey <shock.work@mail.ru>
2020-12-31 04:18:47 +0300

    ability to open and close a modal window

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 const burger = document.querySelector('.burger')
 const menu = document.querySelector('.menu__list')
 const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+const loginButtonsFromMenu = document.querySelectorAll('.header__btn')
+const closeModalButton = document.querySelector('.modal__close')
 
 const toggleElements = (action = 'add') => {
     document.body.classList[action]('lock')
@@ -10,3 +13,17 @@ const toggleElements = (action = 'add') => {
 
 burger.addEventListener('click', () => { toggleElements() })
 overlay.addEventListener('click', () => { toggleElements('remove') })
+
+loginButtonsFromMenu.forEach(item => {
+    item.addEventListener('click', () => {
+        document.body.classList.add('lock')
+        overlay.classList.add('active')
+        modal.classList.add('active')
+    })
+})
+
+closeModalButton.addEventListener('click', () => {
+    document.body.classList.remove('lock')
+    overlay.classList.remove('active')
+    modal.classList.remove('active')
+})
