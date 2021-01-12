ca23fae4acbf3f4d5f3bd86e79e164cfcaa0366a
Andrey <shock.work@mail.ru>
2021-01-12 14:02:15 +0300

    new component breadcrumbs

---
+++ src/components/app/bsBreadcrumbs.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="breadcrumbs__list">
+    <li class="breadcrumbs__item">
+      <a href="#">ГЛАВНАЯ</a>
+    </li>
+    <li class="breadcrumbs__item">
+      <span>ПРАЙС-ЛИСТ</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "bsBreadcrumbs"
+}
+</script>
