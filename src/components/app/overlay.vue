4928a8b2b8da7b8cadb96e0b262371c16db8ac11
Andrey <shock.work@mail.ru>
2021-01-13 14:13:23 +0300

    overlay transform to component

---
+++ src/components/app/overlay.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="overlay" :class="{ active: overlay }" @click="hideAll"></div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex'
+
+export default {
+  name: "overlay",
+  computed: {
+    ...mapGetters(['overlay'])
+  },
+  methods: {
+    ...mapMutations(['hideAll'])
+  }
+}
+</script>
