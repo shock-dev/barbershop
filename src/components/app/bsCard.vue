91ca01c3004b609bd195c7cb98be8f54f1f266a4
Andrey <shock.work@mail.ru>
2021-01-13 10:17:36 +0300

    output products in component

--- src/components/app/bsCard.vue
+++ src/components/app/bsCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="catalog__item card">
     <router-link class="card__img" to="/product">
-      <img src="~@/assets/img/card-img-1.jpg" alt="">
+      <img :src="imageUrl" :alt="title">
     </router-link>
-    <h3 class="card__title">Набор для путешествий «Baxter of California»</h3>
+    <h3 class="card__title">{{ title }}</h3>
     <div class="card__footer">
       <div class="card__price">2 900 ₽</div>
       <button class="btn btn-reset card__btn">Купить</button>
@@ -13,6 +13,11 @@
 
 <script>
 export default {
-  name: "bsCard"
+  name: "bsCard",
+  props: {
+    title: String,
+    price: Number,
+    imageUrl: String
+  }
 }
 </script>
--- src/components/shop/bsCatalog.vue
+++ src/components/shop/bsCatalog.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="catalog">
     <div class="catalog__items">
-      <bs-card v-for="i in 6" :key="i"/>
+      <bs-card
+        v-for="({ title, price, imageUrl }, index) in products"
+        :key="index"
+        :title="title"
+        :price="price"
+        :imageUrl="imageUrl"
+      />
     </div>
     <bs-pagination/>
   </div>
@@ -10,12 +16,16 @@
 <script>
 import BsPagination from "@/components/app/bsPagination";
 import BsCard from "@/components/app/bsCard";
+import { mapGetters } from 'vuex'
 
 export default {
   name: "bsCatalog",
   components: {
     BsCard,
     BsPagination
+  },
+  computed: {
+    ...mapGetters(['products'])
   }
 }
 </script>
